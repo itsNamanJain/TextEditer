99c577e20e1e892212e9ef8bc9ca4ad19e8af572
itsnamanjain <jnaman907@gmail.com>
2022-06-07 12:43:48 +0530

    Adding Detection of False words

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,5 @@
 import React from "react";
- 
+
 export default function Alert(props) {
   const capitalize = (word) => {
     const lower = word.toLowerCase();
--- src/components/Areatext.js
+++ src/components/Areatext.js
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
-
+import FalseWords  from "./Falsewords";
 export default function Areatext(props) {
+  const [count,setCount]=   useState(0);
+
+  // let ListofWords = ["Hello","Bye","Good", "Desk"];
+  // let count=0;
+  const checkWords=()=>{
+    setCount();
+    let words = text.split(" ");
+    let ct=0;
+    for(let i=0;i<words.length;i++){
+      if(FalseWords.includes(words[i].toLowerCase()))
+      ct++;
+    }
+    setCount(ct)
+  }
   const UpperCase = () => {
     let newText = text.toUpperCase();
     setText(newText);
@@ -26,6 +40,7 @@ export default function Areatext(props) {
   };
   const ChangeOn = (event) => {
     setText(event.target.value);
+    // count= checkWords();
   };
   const [text, setText] = useState("");
 
@@ -78,6 +93,13 @@ export default function Areatext(props) {
         >
           Remove Extra Spaces
         </button>
+        <button
+          disabled={text.length === 0}
+          className="btn btn-primary my-3 mx-3"
+          onClick={checkWords}
+        >
+          Check Words
+        </button>
       </div>
       <div className="container my-3">
         <h2>Your Text Summary</h2>
@@ -88,7 +110,9 @@ export default function Areatext(props) {
             }).length
           }{" "}
           words and {text.length} characters.
+
         </p>
+        <p>No of False Words are {count}</p>
         <h2>Preview</h2>
         <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
       </div>
